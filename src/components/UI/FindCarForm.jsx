358d38f540de3b8d1a84b8c89bc52324d32f40fc
piriyaraj <piriyaraj1998@gmail.com>
2022-12-20 23:02:17 +0530

    fitter form setup

--- src/components/UI/FindCarForm.jsx
+++ src/components/UI/FindCarForm.jsx
@@ -4,36 +4,52 @@ import "../../styles/find-car-form.css";
 import { Form, FormGroup } from "reactstrap";
 
 const FindCarForm = () => {
+  const submitHandler = (event) => {
+    var vehicle = event.target.elements.type.value;
+    var district = event.target.elements.district.value;
+  };
   return (
-    <Form className="form">
+    <Form className="form" onSubmit={submitHandler}>
       <div className=" d-flex align-items-center justify-content-between flex-wrap">
-        <FormGroup className="form__group">
-          <input type="text" placeholder="From address" required />
-        </FormGroup>
-
-        <FormGroup className="form__group">
-          <input type="text" placeholder="To address" required />
-        </FormGroup>
-
-        <FormGroup className="form__group">
-          <input type="date" placeholder="Journey date" required />
-        </FormGroup>
-
-        <FormGroup className="form__group">
-          <input
-            className="journey__time"
-            type="time"
-            placeholder="Journey time"
-            required
-          />
-        </FormGroup>
         <FormGroup className="select__group">
-          <select>
-            <option value="ac">AC Car</option>
-            <option value="non-ac">Non AC Car</option>
+          <select name="type">
+            <option value="cycle">Cycle</option>
+            <option value="bike">Bike</option>
+            <option value="van">Van</option>
+            <option value="car">Car</option>
+            <option value="car">Bus</option>
           </select>
         </FormGroup>
 
+        <FormGroup className="select__group">
+          <select name="district">
+            <option value="Colombo">Colombo</option>
+            <option value="Gampaha">Gampaha</option>
+            <option value="Kalutara">Kalutara</option>
+            <option value="Kandy">Kandy</option>
+            <option value="Matale">Matale</option>
+            <option value="NuwaraEliya">Nuwara Eliya</option>
+            <option value="Galle">Galle</option>
+            <option value="Matara">Matara</option>
+            <option value="Hambantota">Hambantota</option>
+            <option value="Jaffna">Jaffna</option>
+            <option value="Kilinochchi">Kilinochchi</option>
+            <option value="Mannar">Mannar</option>
+            <option value="Vavuniya">Vavuniya</option>
+            <option value="Mullaitivu">Mullaitivu</option>
+            <option value="Batticaloa">Batticaloa</option>
+            <option value="Ampara">Ampara</option>
+            <option value="Trincomalee">Trincomalee</option>
+            <option value="Kurunegala">Kurunegala</option>
+            <option value="Puttalam">Puttalam</option>
+            <option value="Anuradhapura">Anuradhapura</option>
+            <option value="Polonnaruwa">Polonnaruwa</option>
+            <option value="Badulla">Badulla</option>
+            <option value="Moneragala">Moneragala</option>
+            <option value="Ratnapura">Ratnapura</option>
+            <option value="Kegalle">Kegalle</option>
+          </select>
+        </FormGroup>
         <FormGroup className="form__group">
           <button className="btn find__car-btn">Find Car</button>
         </FormGroup>
